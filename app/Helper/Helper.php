8ab48a4ce3b2ef3db5e4b0835f788addb84fdf4a
chafiullah <chafiullah@outlook.com>
2025-03-23 02:23:15 +0100

    2025.3.02/10

--- app/Helper/Helper.php
+++ app/Helper/Helper.php
@@ -14,91 +14,96 @@ class Helper
   {
     try {
       if ($percentage > 89) {
-        return ["A+",'CVWA'];
+        return ["A+", 'CVWA'];
       } elseif ($percentage > 79) {
-        return ["A",'CVWA'];
+        return ["A", 'CVWA'];
       } elseif ($percentage > 74) {
-        return ["B+",'CWA'];
+        return ["B+", 'CWA'];
       } elseif ($percentage > 69) {
-        return ["B",'CWA'];
+        return ["B", 'CWA'];
       } elseif ($percentage > 59) {
-        return ["C+",'CA'];
+        return ["C+", 'CA'];
       } elseif ($percentage > 49) {
-        return ["C",'CAA'];
+        return ["C", 'CAA'];
       } else {
-        return ["D",'CNA'];
+        return ["D", 'CNA'];
       }
     } catch (Throwable $th) {
       return $th->getMessage();
     }
-  } 
+  }
   // this function is additionally written because we are sending the average here to calculate the letter grade. Because the average total marks we calculated is the average of marks*coeffcient. That average does not give you the actual figure.
   public static function calculate_average_grade($average)
   {
     try {
       if ($average >= 18.00 && $average <= 20.00) {
-          return ["A+", 'CVWA'];
+        return ["A+", 'CVWA', 'Competences Very Well Acquired'];
       } elseif ($average >= 16.00 && $average < 18.00) {
-          return ["A", 'CVWA'];
+        return ["A", 'CVWA', 'Competences Very Well Acquired'];
       } elseif ($average >= 15.00 && $average < 16.00) {
-          return ["B+", 'CWA'];
+        return ["B+", 'CWA', 'Competences Well Acquired'];
       } elseif ($average >= 14.00 && $average < 15.00) {
-          return ["B", 'CWA'];
+        return ["B", 'CWA', 'Competences Well Acquired'];
       } elseif ($average >= 12.00 && $average < 14.00) {
-          return ["C+", 'CA'];
+        return ["C+", 'CA', 'Competences Acquired'];
       } elseif ($average >= 10.00 && $average < 12.00) {
-          return ["C", 'CAA'];
+        return ["C", 'CAA', 'Competences Averagely Acquired'];
       } else {
-          return ["D", 'CNA'];
+        return ["D", 'CNA', 'Competences Not Acquired'];
       }
     } catch (Throwable $th) {
       return $th->getMessage();
     }
   }
 
-  public static function getActiveAcademicYear(){
-      try {
-          return AcademicYear::where('status','active')->first();
-      } catch (Throwable $th) {
-          return $th->getMessage();
-      }
+  public static function getActiveAcademicYear()
+  {
+    try {
+      return AcademicYear::where('status', 'active')->first();
+    } catch (Throwable $th) {
+      return $th->getMessage();
+    }
   }
 
-  public static function generateStudentUniqueID():string
+  public static function generateStudentUniqueID(): string
   {
     $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
     $charactersLength = strlen($characters);
     $uniqueCode = '';
 
     for ($i = 0; $i < 6; $i++) {
-        $uniqueCode .= $characters[rand(0, $charactersLength - 1)];
+      $uniqueCode .= $characters[rand(0, $charactersLength - 1)];
     }
     $checkValue = StudentInfo::where('student_id', $uniqueCode)->exists();
-    if($checkValue){
-        self::generateStudentUniqueID();
+    if ($checkValue) {
+      self::generateStudentUniqueID();
     }
     return strtoupper($uniqueCode);
   }
 
-  public static function reforgeStudentId($student){
-    $dynamic_part = substr(\Carbon::parse($student->getOriginal('admission_date'))->year,-2);
-    return config('constant.id_heading').$dynamic_part.str_pad($student->id, 3, '0', STR_PAD_LEFT);
+  public static function reforgeStudentId($student)
+  {
+    $dynamic_part = substr(\Carbon::parse($student->getOriginal('admission_date'))->year, -2);
+    return config('constant.id_heading') . $dynamic_part . str_pad($student->id, 3, '0', STR_PAD_LEFT);
   }
 
-  public static function getYear($date_string){
-     return \Carbon::parse($date_string)->year;
+  public static function getYear($date_string)
+  {
+    return \Carbon::parse($date_string)->year;
   }
-  public static function get_listof_subjects($student, $class){
-    $academic_year = PromoteStudent::where('student_id',$student)->where('department_id',$class)->first()->academic_year;
-    $outline = PromoteStudent::where('academic_year',$academic_year)->where('student_id',$student)->where('department_id',$class)->first()->outline_id;
-    $syllabus = OutlineCourse::with('course')->where('outline_id',$outline)->get();
+  public static function get_listof_subjects($student, $class)
+  {
+    $academic_year = PromoteStudent::where('student_id', $student)->where('department_id', $class)->first()->academic_year;
+    $outline = PromoteStudent::where('academic_year', $academic_year)->where('student_id', $student)->where('department_id', $class)->first()->outline_id;
+    $syllabus = OutlineCourse::with('course')->where('outline_id', $outline)->get();
     return $syllabus;
   }
 
-  public static function ordinal($position) {
+  public static function ordinal($position)
+  {
     $ends = ['th', 'st', 'nd', 'rd', 'th', 'th', 'th', 'th', 'th', 'th'];
     if (($position % 100) >= 11 && ($position % 100) <= 13) {
-        return $position . 'th';
+      return $position . 'th';
     }
     return $position . $ends[$position % 10];
   }
--- resources/views/academic/report_card/index.blade.php
+++ resources/views/academic/report_card/index.blade.php
@@ -2,78 +2,90 @@
 
 @section('title', 'Transcripts and Certificates')
 @section('extrastyle')
-  <link href="{{ URL::asset('assets/css/select2.min.css') }}" rel="stylesheet">
-  <link href="{{ URL::asset('assets/css/switchery.min.css') }}" rel="stylesheet">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/11.3.0/sweetalert2.min.css">
+    <link href="{{ URL::asset('assets/css/select2.min.css') }}" rel="stylesheet">
+    <link href="{{ URL::asset('assets/css/switchery.min.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/11.3.0/sweetalert2.min.css">
 @endsection
 
 @section('content')
 
-  <!-- page content -->
-  <div class="right_col" role="main">
-    <div class="">
+    <!-- page content -->
+    <div class="right_col" role="main">
+        <div class="">
 
-      <div class="clearfix"></div>
-      <!-- row start -->
-      <div class="row">
-        <div class="col-sm-12 col-md-12">
-          <div class="x_panel">
-            <div class="x_content">
-              <div class="x_content">
-                <form action="{{ route('academic.report-card.generate') }}" method="post">
-                  @csrf
-                  <div class="form-row">
-                    <div class="form-group col-sm-12 col-md-4">
-                      <label for="">Select Student:</label>
-                      <select name="student_id" class="select2" required>
-                        <option></option>
-                        @foreach ($students as $item)
-                          <option value="{{ $item->id }}">{{ $item->student_id . ' - ' . $item->name }}</option>
-                        @endforeach
-                      </select>
+            <div class="clearfix"></div>
+            <!-- row start -->
+            <div class="row">
+                <div class="col-sm-12 col-md-12">
+                    <div class="x_panel">
+                        <div class="x_content">
+                            <h4 class="text-info">There are a few things you should keep in mind before generating the
+                                report card. It's mandatory.</h4>
+                            <ul class="text-warning">
+                                <li>Make sure you are using <b>Google Chrome/Brave</b> browser</li>
+                                <li>Your file saving type is <b>pdf</b></li>
+                                <li>Margin is <b>default</b></li>
+                            </ul>
+                        </div>
+                        <div class="x_content">
+                            <form action="{{ route('academic.report-card.generate') }}" method="post">
+                                @csrf
+                                <div class="form-row">
+                                    <div class="form-group col-sm-12 col-md-4">
+                                        <label for="">Select Student:</label>
+                                        <select name="student_id" class="select2" required>
+                                            <option></option>
+                                            @foreach ($students as $item)
+                                                <option value="{{ $item->id }}">
+                                                    {{ $item->student_id . ' - ' . $item->name }}</option>
+                                            @endforeach
+                                        </select>
+                                    </div>
+                                    <div class="form-group col-sm-12 col-md-4">
+                                        <label for="">Select Class:</label>
+                                        <select name="class_id" class="select2" required>
+                                            <option></option>
+                                            @foreach ($classes as $item)
+                                                <option value="{{ $item->id }}">{{ $item->name }}</option>
+                                            @endforeach
+                                        </select>
+                                    </div>
+                                    <div class="form-group col-sm-12 col-md-4">
+                                        <label for="">Select Trimester:</label>
+                                        <select name="semester" class="select2" required>
+                                            <option value="{{ config('constant.sem1') }}">{{ config('constant.sem1') }}
+                                            </option>
+                                            <option value="{{ config('constant.sem2') }}">{{ config('constant.sem2') }}
+                                            </option>
+                                            <option value="{{ config('constant.sem3') }}">{{ config('constant.sem3') }}
+                                            </option>
+                                            <option value="{{ config('constant.annual') }}">
+                                                {{ config('constant.annual') }}</option>
+                                        </select>
+                                    </div>
+                                    {{-- additional comments --}}
+                                    @include('academic.components.index_extra')
+                                    <div class="form-group col-sm-12 col-md-12">
+                                        <button type="submit" class="btn btn-success pull-right">generate</button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
                     </div>
-                    <div class="form-group col-sm-12 col-md-4">
-                      <label for="">Select Class:</label>
-                      <select name="class_id" class="select2" required>
-                        <option></option>
-                        @foreach ($classes as $item)
-                          <option value="{{ $item->id }}">{{ $item->name }}</option>
-                        @endforeach
-                      </select>
-                    </div>
-                    <div class="form-group col-sm-12 col-md-4">
-                      <label for="">Select Trimester:</label>
-                      <select name="semester" class="select2" required>
-                        <option value="{{ config('constant.sem1') }}">{{ config('constant.sem1') }}</option>
-                        <option value="{{ config('constant.sem2') }}">{{ config('constant.sem2') }}</option>
-                        <option value="{{ config('constant.sem3') }}">{{ config('constant.sem3') }}</option>
-                        <option value="{{ config('constant.annual') }}">{{ config('constant.annual') }}</option>
-                      </select>
-                    </div>
-                    {{-- additional comments --}}
-                    @include('academic.components.index_extra')
-                    <div class="form-group col-sm-12 col-md-12">
-                      <button type="submit" class="btn btn-success pull-right">generate</button>
-                    </div>
-                  </div>
-                </form>
-              </div>
+                </div>
             </div>
-          </div>
+            <!-- /page content -->
         </div>
-      </div>
-      <!-- /page content -->
     </div>
-  </div>
 @endsection
 @section('extrascript')
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/11.3.0/sweetalert2.min.js"></script>
-  <script>
-    $(document).ready(function() {
-      $(".select2").select2({
-        "placeholder": "Select item..",
-        "allowClear": true
-      });
-    });
-  </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/11.3.0/sweetalert2.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            $(".select2").select2({
+                "placeholder": "Select item..",
+                "allowClear": true
+            });
+        });
+    </script>
 @endsection
