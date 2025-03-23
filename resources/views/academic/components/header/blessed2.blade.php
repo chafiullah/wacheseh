df41d4af1b7f99d64288f13ab9e31e44c85e7690
chafiullah <chafiullah@outlook.com>
2025-03-23 01:14:24 +0100

    2025.3.02/4

--- resources/views/academic/components/header/blessed2.blade.php
+++ resources/views/academic/components/header/blessed2.blade.php
@@ -1,11 +1,10 @@
-
-{{--School information  --}}
+{{-- School information  --}}
 <div class="col-lg-4 col-md-4 col-sm-4">
-{{-- The school information on the left of the report card --}}
-    <span class="header-text">Blessed Anglo-Saxon High School (BASHS) Prestige Simbock</span>
-    <span class="header-text">Discipline - Hard Work - Success. With Jesus At The Centre</span>
-    <span class="header-text">Tel: 676161387, 670090053, 674975975, 677366101</span>
-    <span class="header-text">E-mail: blessedsimbockcollege@gmail.com</span>
+    {{-- The school information on the left of the report card --}}
+    <span class="header-text">Blessed Anglo-Saxon High School (BASHS) Prestige Simbock</span><br>
+    <span class="header-text">Discipline - Hard Work - Success. With Jesus At The Centre</span><br>
+    <span class="header-text">Tel: 676161387, 670090053, 674975975, 677366101</span><br>
+    <span class="header-text">E-mail: blessedsimbockcollege@gmail.com</span><br>
     <span class="header-text">P.O. Box 1839 Simbock - Yaounde</span>
 </div>
 
@@ -14,9 +13,9 @@
 </div>
 
 <div class="col-lg-4 col-md-4 col-sm-4">
-{{-- The school information on the right of the report card --}}
-   <span class="header-text">REPUBLIC OF CAMEROON</span>
-    <span class="header-text">Ministry Of Secondary Education</span>
-    <span class="header-text">Regional Delegation Of Secondary Education</span>
+    {{-- The school information on the right of the report card --}}
+    <span class="header-text">REPUBLIC OF CAMEROON</span><br>
+    <span class="header-text">Ministry Of Secondary Education</span><br>
+    <span class="header-text">Regional Delegation Of Secondary Education</span><br>
     <span class="header-text">Divisional Delegation Secondary Education</span>
 </div>
