3d6a7c3f4a46e80679fe3af5118ce1b7958f08df
chafiullah <chafiullah@outlook.com>
2025-03-23 01:30:46 +0100

    2025.3.02/7

--- resources/views/academic/components/header/blessed2.blade.php
+++ resources/views/academic/components/header/blessed2.blade.php
@@ -1,5 +1,5 @@
 {{-- School information  --}}
-<div class="col-lg-4 col-md-4 col-sm-4">
+<div class="col-lg-5 col-md-5 col-sm-12">
     {{-- The school information on the left of the report card --}}
     <span class="header-text">Blessed Anglo-Saxon High School (BASHS) Prestige Simbock</span><br>
     <span class="header-text">Discipline - Hard Work - Success. With Jesus At The Centre</span><br>
@@ -8,11 +8,11 @@
     <span class="header-text">P.O. Box 1839 Simbock - Yaounde</span>
 </div>
 
-<div class="col-lg-4 col-md-4 col-sm-4 text-center">
+<div class="col-lg-2 col-md-2 col-sm-12 text-center">
     <img src="{{ asset('/images/main_logo.png') }}" alt="main-logo" height="120" width="150" style="margin-top:-2%">
 </div>
 
-<div class="col-lg-4 col-md-4 col-sm-4">
+<div class="col-lg-5 col-md-5 col-sm-12">
     {{-- The school information on the right of the report card --}}
     <span class="header-text">REPUBLIC OF CAMEROON</span><br>
     <span class="header-text">Ministry Of Secondary Education</span><br>
