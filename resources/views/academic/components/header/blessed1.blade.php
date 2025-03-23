3d3b64d249ef0469725a87df41df9ff040a80975
chafiullah <chafiullah@outlook.com>
2025-03-23 01:09:25 +0100

    2025.3.02/3

--- resources/views/academic/components/header/blessed1.blade.php
+++ resources/views/academic/components/header/blessed1.blade.php
@@ -1,10 +1,10 @@
-{{--School information  --}}
+{{-- School information  --}}
 <div class="col-lg-4 col-md-4 col-sm-4">
-{{-- The school information on the left of the report card --}}
-    <span class="header-text">Blessed Anglo-Saxon Comprehensive High School (BASCHS)</span>
-    <span class="header-text">Discipline - Hard Work - Success. With Jesus At The Centre</span>
-    <span class="header-text">Tel: 674975975, 677366101, 652994949, 677558832</span>
-    <span class="header-text">Email: blessedcomprehensivecollege@yahoo.com</span>
+    {{-- The school information on the left of the report card --}}
+    <span class="header-text">Blessed Anglo-Saxon Comprehensive High School (BASCHS)</span> <br>
+    <span class="header-text">Discipline - Hard Work - Success. With Jesus At The Centre</span> <br>
+    <span class="header-text">Tel: 674975975, 677366101, 652994949, 677558832</span> <br>
+    <span class="header-text">Email: blessedcomprehensivecollege@yahoo.com</span> <br>
     <span class="header-text">Nomayos II (Ancien Rail) | P.O. Box 1839 Nomayos Yaounde</span>
 </div>
 
@@ -13,9 +13,9 @@
 </div>
 
 <div class="col-lg-4 col-md-4 col-sm-4">
-{{-- The school information on the right of the report card --}}
-   <span class="header-text">REPUBLIC OF CAMEROON</span>
-    <span class="header-text">Ministry Of Secondary Education</span>
-    <span class="header-text">Regional Delegation Of Secondary Education</span>
+    {{-- The school information on the right of the report card --}}
+    <span class="header-text">REPUBLIC OF CAMEROON</span> <br>
+    <span class="header-text">Ministry Of Secondary Education</span> <br>
+    <span class="header-text">Regional Delegation Of Secondary Education</span> <br>
     <span class="header-text">Divisional Delegation Secondary Education</span>
 </div>
